400370b8ddaa2724a3c1d988e49c2afd30ee7894
v1 <v1@v1s-MacBook-Air.local>
2019-07-02 11:56:18 +0300

    Initial files + express router

---
+++ routes/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+
+// Do work here
+router.get('/', (req, res) => {
+  // const wes = { name: 'wes', age: 100, cool: true };
+
+  // res.send('Hey! It works!');
+  // res.json(wes);
+  // res.send(req.query.name);
+  res.json(req.query);
+  
+});
+
+router.get('/reverse/:name', (req, res) => {
+  const reverse = [...req.params.name].reverse().join('');
+
+  res.send(reverse);
+});
+
+module.exports = router;
