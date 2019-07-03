9f28f54435436276ab0445d3d5fac38ac3422b53
v1 <v1@v1s-MacBook-Air.local>
2019-07-03 16:49:35 +0300

    Middleware and error handling

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,18 +1,16 @@
+exports.myMiddleware = (req, res, next) => {
+  req.siteTitle = 'I like food';
+  res.cookie('name', 'wes is cool', { maxAge: 3000 });
+
+  if (req.siteTitle === 'I like food') {
+    throw Error('Error.. Really??');
+  }
+
+  next();
+}
+
 exports.homePage = (req, res) => {
-    res.render('index', {
-      title: 'I like food',
-    });
-    
-    // const wes = { name: 'wes', age: 100, cool: true };
-  
-    // res.send('Hey! It works!');
-    // res.json(wes);
-    // res.send(req.query.name);
-    // res.json(req.query);
-    
-    // res.render('hello', {
-    //   name: 'wes',
-    //   dog: req.query.dog,
-    //   title: 'I like food'
-    // });
-  };
+  res.render('index', {
+    title: req.siteTitle,
+  });
+};
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const storeController = require('../controllers/storeController')
 
 // Do work here
-router.get('/', storeController.homePage);
+router.get('/', storeController.myMiddleware, storeController.homePage);
 
 router.get('/reverse/:name', (req, res) => {
   const reverse = [...req.params.name].reverse().join('');
