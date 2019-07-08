fd18987138406c0ffe25eda0b6b6017dd2e8b508
v1 <v1@v1s-MacBook-Air.local>
2019-07-08 09:12:35 +0300

    Async await

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,3 +1,6 @@
+const mongoose = require('mongoose');
+const Store = mongoose.model('Store');
+
 exports.homePage = (req, res) => {
   res.render('index', {
     title: req.siteTitle,
@@ -10,6 +13,11 @@ exports.addStore = (req, res) => {
   })
 };
 
-exports.createStore = (req, res) => {
-  res.json(req.body)
+exports.createStore = async (req, res) => {
+  const store = new Store(req.body);
+
+  await store.save();
+  res.redirect('/');
+  
+  console.log('it works!');
 };
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const storeController = require('../controllers/storeController')
+const { catchErrors } = require('../handlers/errorHandlers');
 
 // Do work here
 router.get('/', storeController.homePage);
 router.get('/add', storeController.addStore);
-router.post('/add', storeController.createStore);
+router.post('/add', catchErrors(storeController.createStore));
 
 // router.get('/reverse/:name', (req, res) => {
 //   const reverse = [...req.params.name].reverse().join('');
