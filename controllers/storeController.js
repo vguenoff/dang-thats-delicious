eff44ed976a2a7ebf1ac89c0d565e907068ec763
v1 <v1@v1s-MacBook-Air.local>
2019-07-10 01:52:32 +0300

    Querying our Database for Stores

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -26,3 +26,10 @@ exports.createStore = async (req, res) => {
   req.flash('success', `Succesfully created ${store.name}. Care to leave a review?`);
   res.redirect(`/store/${store.slug}`);
 };
+
+exports.getStores = async (req, res) => {
+  const stores = await Store.find();
+  // console.log(stores);
+
+  res.render('Stores', { title: 'Stores', stores });
+};
